a13ace7624db216cd9aa037e80aa6ae6389efe0a
arch-spatula <shkim516@naver.com>
2023-01-21 22:44:50 +0900

    feat: 모든 목록 반영 #13
    화면에 해당하는 모든 목록을 보여줍니다.
    해당하는 level에 라우팅할 수 있습니다.

--- src/components/MainList.tsx
+++ src/components/MainList.tsx
@@ -26,6 +26,79 @@ const tutorialList: tutorialListType[] = [
     title: "라이브러리 심화",
     techStackIcon: [{ src: "React", alt: "React.js" }],
   },
+  {
+    title: "Style",
+    techStackIcon: [
+      { src: "Styled-Components", alt: "Styled-Components" },
+      { src: "TailwindCSS", alt: "Tailwind CSS" },
+    ],
+  },
+  {
+    title: "상태 관리 라이브러리",
+    techStackIcon: [
+      { src: "Redux", alt: "Redux" },
+      { src: "Recoil", alt: "Recoil" },
+      { src: "React-Query", alt: "React-Query" },
+      { src: "Zustand", alt: "Zustand" },
+    ],
+  },
+  {
+    title: "라우터",
+    techStackIcon: [
+      { src: "React-Router-Dom", alt: "React-Router-Dom" },
+      { src: "TanStack-Router", alt: "TanStack-Router" },
+    ],
+  },
+  {
+    title: "타입스크립트",
+    techStackIcon: [
+      { src: "TypeScript", alt: "TypeScript" },
+      { src: "React", alt: "React" },
+    ],
+  },
+  {
+    title: "Next와 서버사이드 렌더링",
+    techStackIcon: [{ src: "Next", alt: "Next.js" }],
+  },
+  {
+    title: "테스팅",
+    techStackIcon: [
+      { src: "Jest", alt: "Jest.js" },
+      { src: "Testing-Library", alt: "Testing-Library" },
+    ],
+  },
+  {
+    title: "데이터 베이스",
+    techStackIcon: [
+      { src: "MongoDB", alt: "MongoDB" },
+      { src: "MySQL", alt: "MySQL" },
+      { src: "Graphql", alt: "Grapql" },
+      { src: "Supabase", alt: "Supabase" },
+    ],
+  },
+  {
+    title: "클라우드",
+    techStackIcon: [
+      { src: "AWS", alt: "AWS" },
+      { src: "Firebase", alt: "MySQL" },
+      { src: "CloudFlare", alt: "CloudFlare" },
+    ],
+  },
+  {
+    title: "모듈 번들러",
+    techStackIcon: [
+      { src: "Webpack", alt: "Webpack" },
+      { src: "Vite", alt: "Vite" },
+      { src: "Turbopack", alt: "Turbopack" },
+    ],
+  },
+  {
+    title: "버전관리와 협업",
+    techStackIcon: [
+      { src: "Git", alt: "Git" },
+      { src: "Github", alt: "Github" },
+    ],
+  },
 ];
 
 const MainList = () => {
