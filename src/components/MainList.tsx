16f984ee3c932fc9229cd26a05d63a81496e986e
arch-spatula <shkim516@naver.com>
2023-01-21 20:46:36 +0900

    refactor: 튜토리얼 목록 컴포넌트화 #13
    튜토리얼 목록들을 컴포넌트화 했습니다.
    개별 목록의 아이콘도 컴포넌트화 했습니다.
    개별목록화 하는 과정에서 아이콘 컴포넌트를 타입으로 설계했습니다.

--- src/components/MainList.tsx
+++ src/components/MainList.tsx
@@ -1,58 +1,44 @@
 import MainListItem from "./MainListItem";
-import { Link } from "react-router-dom";
-// import CF from "icon/CF.png";
 
-type MainListProps = {
-  items: [];
+type tutorialListType = {
+  title: string;
+  techStackIcon: TechIconImg[];
 };
 
-const tutorialList = [{}];
-
-{
-  /* {tutorialList.map((item, idx) => <li id={idx.toString()}></li>)} */
-}
+const tutorialList: tutorialListType[] = [
+  {
+    title: "기초공사",
+    techStackIcon: [
+      { src: "HTML5", alt: "HTML 5" },
+      { src: "CSS3", alt: "CSS 3" },
+      { src: "JavaScript", alt: "JavaScript" },
+    ],
+  },
+  {
+    title: "라이브러리",
+    techStackIcon: [
+      { src: "Angular", alt: "Garbage" },
+      { src: "Vue", alt: "Vue.js" },
+      { src: "React", alt: "React.js" },
+    ],
+  },
+  {
+    title: "라이브러리 심화",
+    techStackIcon: [{ src: "React", alt: "React.js" }],
+  },
+];
 
 const MainList = () => {
   return (
     <ul className="max-w-screen-xl my-0 mx-auto flex flex-col gap-10">
-      <li>
-        <Link to={"1"}>
-          <div className="flex flex-grow bg-stone-800 p-8 mx-4 rounded-[2rem] h-64 items-center justify-between">
-            <div className="flex gap-3 flex-col">
-              <p className="text-white text-2xl">지옥문 1단계</p>
-              <h3 className="text-white font-bold text-5xl">기초공사</h3>
-            </div>
-            <div className="flex flex-row gap-3">
-              <img className="w-20 h-20" src="/icons/html5.png" alt="HTML" />
-              <img className="w-20 h-20" src="/icons/css3.png" alt="CSS" />
-              <img
-                className="w-20 h-20"
-                src="/icons/javascript.png"
-                alt="JavaScript"
-              />
-            </div>
-          </div>
-        </Link>
-      </li>
-      <li>
-        <Link to={"2"}>
-          <div className="flex flex-grow bg-stone-800 p-8 mx-4 rounded-[2rem] h-64 items-center justify-between">
-            <div className="flex gap-3 flex-col">
-              <p className="text-white text-2xl">지옥문 2단계</p>
-              <h3 className="text-white font-bold text-5xl">라이브러리</h3>
-            </div>
-            <div className="flex flex-row gap-3">
-              <img
-                className="w-20 h-20"
-                src="/icons/angular.png"
-                alt="garbage"
-              />
-              <img className="w-20 h-20" src="/icons/vue.png" alt="Vue" />
-              <img className="w-20 h-20" src="/icons/react.png" alt="React" />
-            </div>
-          </div>
-        </Link>
-      </li>
+      {tutorialList.map(({ title, techStackIcon }, idx) => (
+        <MainListItem
+          key={idx}
+          title={title}
+          idx={idx}
+          techStackIcons={techStackIcon}
+        />
+      ))}
     </ul>
   );
 };
