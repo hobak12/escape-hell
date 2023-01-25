0fcc7ed4cf9a1c0bdc82fabb3aebcf612b715cbb
arch-spatula <shkim516@naver.com>
2023-01-25 11:19:49 +0900

    refactor: Suspense 작업 보류 #12
    Suspense작을 잠시 보류하고 코드를 작성할 위치만 표시합니다.

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { ScrollingCarousel } from "@trendyol-js/react-carousel";
-// import ArrowButton from "./ArrowButton";
 
 type VideoListProps = {
   title: string;
   list: string[];
 };
 
+/**
+ * @see https://tanstack.com/query/v4/docs/react/guides/suspense
+ */
 const VideoList = ({ title, list }: VideoListProps) => {
   return (
     <div className="pt-6">
       <h2 className="text-white text-3xl font-bold mb-3 mx-3">{title}</h2>
-      <ScrollingCarousel
-        className="flex"
-        // rightIcon={<ArrowButton direction="right" />}
-        // leftIcon={<ArrowButton direction="left" />}
-      >
-        {list.map((item) => (
-          <div key={item} className="mx-3">
-            <iframe
-              className="rounded h-48 aspect-video"
-              src={item}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </div>
-        ))}
+      <ScrollingCarousel className="flex">
+        {list.map((item) => {
+          return (
+            <div key={item} className="mx-3">
+              <iframe
+                className="rounded h-48 aspect-video"
+                src={item}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
+          );
+        })}
       </ScrollingCarousel>
     </div>
   );
--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,29 +1,24 @@
-import { lazy, Suspense } from "react";
-// import { VideoList } from "../components";
-
-const VideoList = lazy(() => import("../components/VideoList"));
-
-const renderLoader = () => <p>Loading</p>;
-
+import { VideoList } from "../components";
 import CommentInputs from "../components/Comment/CommentInputs";
 import CommentList from "../components/Comment/CommentList";
 
 const Tutorial = () => {
   return (
     <div className="bg-neutral-900 min-h-screen">
+      {/* header 컴포넌트 자리 */}
       <div className="mx-32 my-0">
-        <Suspense fallback={renderLoader()}>
-          <VideoList
-            title="JavaScript"
-            list={[
-              "https://www.youtube.com/embed/Uo3cL4nrGOk",
-              "https://www.youtube.com/embed/14jlIxVrcvo",
-              "https://www.youtube.com/embed/1UXHsCT18wE",
-              "https://www.youtube.com/embed/bXzTXD_OJo0",
-              "https://www.youtube.com/embed/NtfbWkxJTHw",
-            ]}
-          />
-        </Suspense>
+        <VideoList
+          title="JavaScript"
+          list={[
+            "https://www.youtube.com/embed/Uo3cL4nrGOk",
+            "https://www.youtube.com/embed/14jlIxVrcvo",
+            "https://www.youtube.com/embed/1UXHsCT18wE",
+            "https://www.youtube.com/embed/bXzTXD_OJo0",
+            "https://www.youtube.com/embed/NtfbWkxJTHw",
+            "https://www.youtube.com/embed/NtfbWkxJTHw",
+            "https://www.youtube.com/embed/LbmvbXPj8Fs",
+          ]}
+        />
         <CommentInputs />
         <CommentList />
       </div>
