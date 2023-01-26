402ebbc8f97365c3b7867c0d285e9796728801a9
JeonInguk <inguk12951@gmail.com>
2023-01-26 21:23:11 +0900

    feat: 모달 첫 커밋 #1 #3 #7

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -4,36 +4,80 @@ import CommentList from "../components/Comment/CommentList";
 import Header from "../components/Header/Header";
 import { useParams } from "react-router-dom";
 import tutorial from "../data/tutorialList.json";
+//
+import checkList from "../data/checkYourSelf.json";
+import { useEffect, useState } from "react";
+import Modal from "../components/Modal";
 
 const Tutorial = () => {
   const { level } = useParams();
   const { title, techStackIcon } = tutorial[parseInt(level!) - 1];
+  const checkListJson = checkList[parseInt(level!) - 1];
+  const [userCheck, setUserCheck] = useState(checkListJson);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const setDone = (id: number) => {
+    const newChecked = [...checkListJson];
+    const idx = newChecked.findIndex((t) => t.id === id);
+    newChecked[idx].isDone = !newChecked[idx].isDone;
+    setUserCheck(newChecked);
+  };
+
+  useEffect(() => {
+    const getData = () => {
+      const local_check = localStorage.getItem("checked");
+      if (local_check?.length === 0) {
+        setUserCheck(checkListJson);
+      } else {
+        setUserCheck(JSON.parse(local_check!));
+      }
+    };
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const saveCheckList = () => {
+      localStorage.setItem("checked", JSON.stringify(userCheck));
+    };
+    saveCheckList();
+  }, [userCheck]);
 
   return (
     <>
       <div className="bg-neutral-900 min-h-screen">
         <Header />
-        <div className="mx-32 my-0 py-12">
-          <h1 className="text-white text-6xl font-bold mx-3 my-8">{title}</h1>
-          {techStackIcon.map((item) => (
-            <VideoList
-              key={item.src}
-              title={item.alt}
-              src={item.src}
-              list={[
-                "https://www.youtube.com/embed/Uo3cL4nrGOk",
-                "https://www.youtube.com/embed/14jlIxVrcvo",
-                "https://www.youtube.com/embed/1UXHsCT18wE",
-                "https://www.youtube.com/embed/bXzTXD_OJo0",
-                "https://www.youtube.com/embed/NtfbWkxJTHw",
-                "https://www.youtube.com/embed/LbmvbXPj8Fs",
-              ]}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="bg-default flex-column text-white px-10 pt-2 pb-10">
-        <div className="bg-header h-px" />
+        <h1 className="text-white text-6xl font-bold mx-3 my-8">{title}</h1>
+        {techStackIcon.map((item) => (
+          <VideoList
+            level={level!}
+            key={item.src}
+            title={item.alt}
+            src={item.src}
+            url={item.url}
+          />
+        ))}
+        <button onClick={openModal}>모달팝업</button>
+        <Modal open={modalOpen} close={closeModal} header="Check yourslef!">
+          <div>
+            {userCheck?.map((item) => (
+              <div key={item.id}>
+                <input
+                  type={"checkbox"}
+                  onChange={() => setDone(item.id)}
+                  checked={item.isDone}
+                />
+                {item.content}
+              </div>
+            ))}
+          </div>
+        </Modal>
         <CommentInputs />
         <div className="bg-header h-px" />
         <CommentList />
