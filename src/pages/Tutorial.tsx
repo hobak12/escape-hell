4ba70ed16236b93854ec57f5caa3ece6984fd253
hobak12 <tjsgud2564@naver.com>
2023-01-27 15:12:46 +0900

    design: 모달 버튼 css #1 #3

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -70,18 +70,23 @@ const Tutorial = () => {
     <>
       <div className="bg-neutral-900 min-h-screen">
         <Header />
-        <h1 className="text-white text-6xl font-bold mx-3 my-8">{title}</h1>
-        {techStackIcon.map((item) => (
-          <VideoList
-            level={level!}
-            key={item.src}
-            title={item.alt}
-            src={item.src}
-            url={item.url}
-          />
-        ))}
-        <button className="text-white" onClick={openModal}>
-          모달팝업
+        <div className="px-20">
+          <h1 className="text-white text-6xl font-bold mx-3 my-8">{title}</h1>
+          {techStackIcon.map((item) => (
+            <VideoList
+              level={level!}
+              key={item.src}
+              title={item.alt}
+              src={item.src}
+              url={item.url}
+            />
+          ))}
+        </div>
+        <button
+          onClick={openModal}
+          className="fixed right-5 bottom-5 bg-red-600 rounded-full p-2.5 w-10 h-10"
+        >
+          <img alt="checklist" src="../icons/checklist.png" />
         </button>
         <Modal open={modalOpen} close={closeModal} header="Check yourslef!">
           <div>
@@ -97,7 +102,7 @@ const Tutorial = () => {
             ))}
           </div>
         </Modal>
-        <div className="bg-default flex-column text-white px-10 pt-2 pb-10">
+        <div className="bg-default flex-column text-white px-20 pt-2 pb-10">
           <div className="bg-header h-px" />
           <CommentInputs />
           <div className="bg-header h-px" />
