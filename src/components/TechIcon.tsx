9923b675626ee1af642d0f6590ebdb053ffba407
arch-spatula <shkim516@naver.com>
2023-01-27 20:55:07 +0900

    fix: 이미지 없음 문제 url로 불러오기 #2

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -1,5 +1,5 @@
 const TechIcon = ({ src, alt, className = "w-20 h-20" }: TechIconImg) => {
-  return <img className={className} src={`/icons/${src}.png`} alt={alt} />;
+  return <img className={className} src={src} alt={alt} />;
 };
 
 export default TechIcon;
